52dca478618177e8ad7f73c14afd050160e7c94f
睿 蔡 <RayChromium@outlook.com>
2019-12-08 21:10:05 +0800

    Scoring logic  and win condition

--- WerdGame/Main.cpp
+++ WerdGame/Main.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <iostream>
 #include <random>
+#include <algorithm>
 
 bool vector_contain_word(const std::vector<std::string>& vec, const std::string& word)
 {
@@ -14,6 +15,42 @@ bool vector_contain_word(const std::vector<std::string>& vec, const std::string&
 		}
 	}
 	return false;
+}
+
+std::vector<int> fill_buckets(const std::string& word)
+{
+	//Each letter in the alphabet matches an entry, which is the amount in the word
+	std::vector<int> bucket(26, 0);
+	for (const auto& c : word)
+	{
+		bucket[c - 'a']++;
+	}
+	return bucket;
+}
+
+//Break the scoring proccess to 2 steps:
+//1. Fill the buckets to see how many letters matched
+//2. Check if they're in the right position
+int score_match(const std::string& word1, const std::string& word2)
+{
+	const auto bucket1 = fill_buckets(word1);
+	const auto bucket2 = fill_buckets(word2);
+	//take the minimum of the corresponding entry in the bucket as the matching score
+	int score = 0;
+	for (int i = 0; i < 26; ++i)
+	{
+		score += std::min(bucket1[i], bucket2[i]);
+	}
+	
+	//Now, check the positions:
+	for (int i = 0; i < 5; ++i)
+	{
+		if (word1[i] == word2[i])
+		{
+			score++;
+		}
+	}
+	return score;
 }
 
 int main()
@@ -57,8 +94,23 @@ int main()
 			std::cout << "? That's not even a god damnded word, stupid asshole." << std::endl;
 			continue;
 		}
+		
+		const int score = score_match(guess, target);
+		if (score == 10)
+		{
+			std::cout << "You did it.WOW." << std::endl;
+			break;
+		}
+		else
+		{
+			std::cout << "Nah that ain't it. I'll give you " << score << " points"<<std::endl;
+			continue;
+		}
 
 	}
 
+	std::cin.clear();
+	std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+	std::cin.get();
 	return 0;
 }
